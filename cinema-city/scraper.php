38c661f075faa3963a1a08ec083c67d1559c6430
Adrian Zmenda <adiq@adiq.eu>
2017-10-31 21:28:22 +0100

    Add multiple cinemas

--- cinema-city/scraper.php
+++ cinema-city/scraper.php
@@ -1,5 +1,10 @@
 <?php
 
+$cinemas = [
+    'Bytom' => 1092,
+    'Bielsko-Biala' => 1088
+];
+
 function getDaySchedule($cinemaId, $date = null) {
 
     $scheduleJson = file_get_contents('https://www.cinema-city.pl/pl/data-api-service/v1/quickbook/10103/film-events/in-cinema/'.$cinemaId.'/at-date/'.$date.'?attr=&lang=pl_PL');
@@ -7,4 +12,6 @@ function getDaySchedule($cinemaId, $date = null) {
     return json_decode($scheduleJson, true);
 }
 
-var_dump(getDaySchedule(1092, date('Y-m-d')));
+foreach ($cinemas as $cinema) {
+    var_dump(getDaySchedule($cinema, date('Y-m-d')));
+}

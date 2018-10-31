f5e6e51363b335c370b522d697734af3c5669f02
Adrian Zmenda <adrian@zmenda.com>
2018-10-31 20:04:30 +0100

    Rework application to work on models

--- app.php
+++ app.php
@@ -2,7 +2,16 @@
 
 require 'vendor/autoload.php';
 
+use App\Enum\CinemaType;
+use App\Model\CinemaModel;
 use App\Provider\CinemaCity;
 
+$cinemaCity = new CinemaCity();
+$cinemaCityCinemas = [
+    new CinemaModel(1092, 'Bytom', CinemaType::CINEMA_CITY),
+    new CinemaModel(1088, 'Bielsko-Biała', CinemaType::CINEMA_CITY)
+];
 
-new CinemaCity();
+foreach ($cinemaCityCinemas as $cinema) {
+    var_dump($cinemaCity->getScheduleForCinema($cinema));
+}
--- src/Provider/CinemaCity.php
+++ src/Provider/CinemaCity.php
@@ -2,32 +2,61 @@
 
 namespace App\Provider;
 
+use App\Model\CinemaModel;
+use App\Model\EventModel;
+use App\Model\MovieModel;
+
 class CinemaCity {
-    public function __construct()
-    {
-        $cinemas = [
-            'Bytom' => 1092,
-            'Bielsko-Biala' => 1088
-        ];
+    public function getScheduleForCinema(CinemaModel $cinemaModel) {
+        $dates = $this->getAvailableScheduleDays($cinemaModel->getId(), date('Y-m-d'));
 
-        function getDaySchedule($cinemaId, $date = null) {
+        foreach ($dates['body']['dates'] as $date) {
+            $schedule = $this->getDaySchedule($cinemaModel->getId(), $date);
+            foreach ($schedule['body']['films'] ?? [] as $movieData) {
+                $movie = $this->mapMovie($movieData);
+                $this->mapEvents($movie, $schedule['body']['events'] ?? []);
 
-            $scheduleJson = file_get_contents('https://www.cinema-city.pl/pl/data-api-service/v1/quickbook/10103/film-events/in-cinema/'.$cinemaId.'/at-date/'.$date.'?attr=&lang=pl_PL');
-
-            return json_decode($scheduleJson, true);
-        }
-
-
-        function getAvailableScheduleDays($cinemaId, $date = null) {
-            $json = file_get_contents('https://www.cinema-city.pl/pl/data-api-service/v1/quickbook/10103/dates/in-cinema/'.$cinemaId.'/until/'.$date.'?attr=&lang=pl_PL');
-            return json_decode($json, true);
-        }
-
-        foreach ($cinemas as $cinema) {
-            $dates = getAvailableScheduleDays($cinema, date('2018-m-d'));
-            foreach ($dates['body']['dates'] as $date) {
-                var_dump(getDaySchedule($cinema, $date));
+                $cinemaModel->addMovie($movie);
             }
         }
+
+        return $cinemaModel;
+    }
+
+    private function mapMovie($data) {
+        return new MovieModel(
+        $data['id'] ?? null,
+            $data['name'] ?? null,
+            $data['length'] ?? null,
+            $data['posterLink'] ?? null,
+            $data['videoLink'] ?? null,
+            $data['link'] ?? null,
+            $data['attributeIds'] ?? []
+        );
+    }
+
+    private function mapEvents(MovieModel $movie, $events) {
+        foreach ($events as $event) {
+            if ($event['filmId'] !== $movie->getId()) {
+                continue;
+            }
+            $movie->addEvent(new EventModel(
+                new \DateTime($event['eventDateTime']),
+                $event['bookingLink'] ?? null,
+                $event['soldOut'] ?? null
+            ));
+        }
+    }
+
+    private function getDaySchedule($cinemaId, $date = null) {
+        $scheduleJson = file_get_contents('https://www.cinema-city.pl/pl/data-api-service/v1/quickbook/10103/film-events/in-cinema/'.$cinemaId.'/at-date/'.$date.'?attr=&lang=pl_PL');
+
+        return json_decode($scheduleJson, true);
+    }
+
+    private function getAvailableScheduleDays($cinemaId, $date = null) {
+        $json = file_get_contents('https://www.cinema-city.pl/pl/data-api-service/v1/quickbook/10103/dates/in-cinema/'.$cinemaId.'/until/'.$date.'?attr=&lang=pl_PL');
+
+        return json_decode($json, true);
     }
 }
